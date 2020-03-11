ba735023c899cfa76a47512a66e4881e74183edf
rjdirisio <rjdiri@uw.edu>
2020-03-11 14:59:44 -0700

    Put it all in researchCode

--- DMC_General.py
+++ DMC_General.py
@@ -1,31 +1,7 @@
 import numpy as np
 import os
 import time
-
-class Constants:
-    atomic_units = {
-        "wavenumbers" : 4.55634e-6,
-        "angstroms" : 1/0.529177,
-        "amu" : 1.000000000000000000/6.02213670000e23/9.10938970000e-28   #1822.88839  g/mol -> a.u.
-    }
-
-    masses = {
-        "H" : ( 1.00782503223, "amu"),
-        "O" : (15.99491561957, "amu"),
-        "D" : (2.0141017778,"amu")
-    }
-    @classmethod
-    def convert(cls, val, unit, to_AU = True):
-        vv = cls.atomic_units[unit]
-        return (val * vv) if to_AU else (val / vv)
-
-    @classmethod
-    def mass(cls, atom, to_AU = True):
-        m = cls.masses[atom]
-        if to_AU:
-            m = cls.convert(*m)
-        return m
-
+from ..researchUtils import Constants
 
 class DMC:
     def __init__(self,
